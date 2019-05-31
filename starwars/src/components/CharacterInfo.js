fc7f12c79be7a534239e2541ab59a2d899cc5ba9
nanoblit <nanoblit@users.noreply.github.com>
2019-05-31 11:45:25 +0200

    Fix showing links

--- starwars/src/components/CharacterInfo.js
+++ starwars/src/components/CharacterInfo.js
@@ -6,10 +6,16 @@ const CharacterInfo = ({ data }) => (
     {data.birth_year === 'unknown' ? '' : <p>Born in {data.birth_year}</p>}
     <p>
       They are {data.gender === 'n/a' ? '' : data.gender}{' '}
-      {data.species ? data.species.map(species => species) : ''}
+      {data.species
+        ? data.species.map(species => (species.indexOf('https://') > -1 ? '' : species))
+        : ''}
     </p>
     <h2>Starred in:</h2>
-    <ul>{data.films ? data.films.map(film => <li key={film}>{film}</li>) : ''}</ul>
+    <ul>
+      {data.films
+        ? data.films.map(film => <li key={film}>{film.indexOf('https://') > -1 ? '' : film}</li>)
+        : ''}
+    </ul>
   </div>
 );
 
