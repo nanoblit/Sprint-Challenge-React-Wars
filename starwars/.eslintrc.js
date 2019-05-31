ce6a0a08e051705a2d4c139b4a07bc476f8d01a2
nanoblit <nanoblit@users.noreply.github.com>
2019-05-31 08:57:01 +0200

    Commit before eject

---
+++ starwars/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: "airbnb",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "babel"],
+  rules: {
+    "react/jsx-filename-extension": 0,
+    "linebreak-style": 0,
+    "react/prop-types": 0,
+    "no-invalid-this": 0,
+    "babel/no-invalid-this": 1,
+    "arrow-parens": 0,
+    "no-debugger": 0,
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "jsx-a11y/click-events-have-key-events": 0
+  }
+};
